b718433da8cdd1ef5ec7e70dcf5f0482a3ae9a31
Ganjou <102141564+Ganjou@users.noreply.github.com>
2023-10-10 17:48:09 +0700

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.css';
+	import '@fontsource-variable/open-sans';
+	import '@fontsource/poppins';
+	import '@fontsource-variable/inter';
+
+	import Navbar from '$lib/Navbar.svelte';
+</script>
+
+<header>
+	<Navbar />
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style lang="postcss">
+	:global(body) {
+		@apply w-full min-h-screen font-landing;
+	}
+</style>
